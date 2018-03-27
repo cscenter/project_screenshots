30198bfeafe62c8fd57f13ad2a38ed8fdcb3eb73
amosov-f <amosov-f@yandex-team.ru>
2018-03-28 00:14:46 +0300

    setup.py for pip install test

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
     version="1.0.0",
     description="Quality of SERP Screenshots",
     packages=find_packages(),
-    # install_requires=['numpy', 'opencv-python'],
-    requires=['numpy', 'opencv-python']
+    install_requires=['numpy', 'opencv-python'],
+    # requires=['numpy', 'opencv-python']
 )

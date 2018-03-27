9323c0a2edbfcd4a1d6a04b6d9ad2d991f3f2640
amosov-f <amosov-f@yandex-team.ru>
2018-03-27 21:20:55 +0300

    setup.py for pip install

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name="screenshot-quality",
+    version="1.0.0",
+    description="Quality of SERP Screenshots",
+    packages=['src/screenshots', 'src/screenshots/core', 'src/screenshots/screenshot_filters'],
+    install_requires=['numpy', 'opencv-python'],
+    requires=['numpy', 'opencv-python']
+)

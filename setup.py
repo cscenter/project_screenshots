c2f6cd5e635d033fbfcb526914c64bf8574a2c22
amosov-f <amosov-f@yandex-team.ru>
2018-03-28 01:12:02 +0300

    setup.py for pip install correct structure

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="screenqual",
+    version="1.0.0",
+    description="Quality of SERP Screenshots",
+    packages=find_packages(exclude=("tests",)),
+    install_requires=['numpy', 'opencv-python']
+)

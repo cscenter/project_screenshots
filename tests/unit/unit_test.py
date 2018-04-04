6b34bf17fdbe25a6f6768f079dcb33e161e69500
sabrinamusatian <sabrina.musatian@yandex.ru>
2018-04-04 16:30:47 +0300

    Fix unit tests for new changes

--- tests/unit/unit_test.py
+++ tests/unit/unit_test.py
@@ -8,12 +8,12 @@ from screenqual.core.screenshot import Screenshot
 # Base class for all unit tests run on different filters
 class UnitTest(unittest.TestCase):
     def assert_has_anomaly(self, analyser, relative_path):
-        self.assertTrue(self._analyze(analyser, relative_path))
+        self.assertTrue(self._analyze(analyser, relative_path).has_anomaly)
 
     def assert_no_anomaly(self, analyser, relative_path):
-        self.assertFalse(self._analyze(analyser, relative_path))
+        self.assertFalse(self._analyze(analyser, relative_path).has_anomaly)
 
     def _analyze(self, analyser, relative_path):
         base_path = os.path.join(os.path.dirname(__file__))
         img = cv2.imread(base_path + "/" + relative_path)
-        return analyser.execute(Screenshot(img, None, None, []))
+        return analyser.execute(Screenshot(img))

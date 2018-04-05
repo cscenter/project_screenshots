161e99ad1db16747473fa81dd7602d113fa31c61
Vladimir Nazarenko <nazarenko2010@gmail.com>
2018-04-05 23:31:16 +0300

    Adds the sketch implementation ov vertical lines crossing detection

--- screenqual/filter/text_near_edge_detector.py
+++ screenqual/filter/text_near_edge_detector.py
@@ -5,8 +5,8 @@ from screenqual.core.analyser_result import AnalyserResult
 
 
 class TextNearEdgeDetector(ScreenshotAnalyser):
-    tolerance = 0.1
-    frame_height = 5
+    tolerance = 0.01
+    frame_height = 2
     min_pixels_in_line_ratio = 0.01
 
     def execute(self, screenshot):
@@ -14,14 +14,30 @@ class TextNearEdgeDetector(ScreenshotAnalyser):
         assert img.shape[0] > 2 * self.frame_height, "Img is too narrow (height is {0} pixels)".format(img.shape[0])
         img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
         img = 255 - img[..., 2]
-        # check top and bottom
+        has_text_near_horizontal_edge = self._check_horizontal_edges(img)
+        has_text_near_vertical_edge   = self._check_vertical_edges(img)
+
+        if has_text_near_horizontal_edge and has_text_near_vertical_edge:
+            return AnalyserResult.with_anomaly({"cause": "Horizontal and vertical border crosses the text"})
+        elif has_text_near_vertical_edge:
+            return AnalyserResult.with_anomaly({"cause": "Vertical border crosses the text"})
+        elif has_text_near_horizontal_edge:
+            return AnalyserResult.with_anomaly({"cause": "Horizontal border crosses the text"})
+        else:
+            return AnalyserResult.without_anomaly()
+
+    def _check_horizontal_edges(self, img):
         row_sums = np.sum(img, axis=1)
         min_pixels_in_line = img.shape[1] * self.min_pixels_in_line_ratio
         median_row_sum = np.median(row_sums[row_sums > min_pixels_in_line])
         head = row_sums[:self.frame_height]
         tail = row_sums[-self.frame_height:]
-        has_text_near_edge = max(np.max(head), np.max(tail)) > self.tolerance * median_row_sum
-        if has_text_near_edge:
-            return AnalyserResult.with_anomaly({"cause": "Horizontal border crosses the text"})
-        else:
-            return AnalyserResult.without_anomaly()
+        return max(np.max(head), np.max(tail)) > self.tolerance * median_row_sum
+
+    def _check_vertical_edges(self, img):
+        col_sums = np.sum(img, axis=0)
+        min_pixels_in_col = img.shape[0] * self.min_pixels_in_line_ratio
+        median_col_sum = np.median(col_sums[col_sums > min_pixels_in_col])
+        head = col_sums[:self.frame_height]
+        tail = col_sums[-self.frame_height:]
+        return max(np.max(head), np.max(tail)) > self.tolerance * median_col_sum
--- tests/unit/text_near_edge/test_text_near_edge.py
+++ tests/unit/text_near_edge/test_text_near_edge.py
@@ -6,10 +6,14 @@ class TestTextNearEdgeDetector(UnitTest):
     def setUp(self):
         self.dtor = TextNearEdgeDetector()
 
-    def test_does_not_fire_on_good_text_document_screenshots(self):
+    def test_does_not_fire_on_good_text_document_screenshot(self):
         self.assert_no_anomaly(self.dtor,
                                "text_near_edge/ok.png")
 
-    def test_fires_on_bad_text_document_screenshots(self):
+    def test_fires_on_text_document_screenshot_with_text_crossing_horizontal_line(self):
         self.assert_has_anomaly(self.dtor,
-                                "text_near_edge/text_on_edge.png")
+                                "text_near_edge/horizontal.png")
+
+    def test_fires_on_text_document_screenshot_with_text_crossing_vertical_line(self):
+        self.assert_has_anomaly(self.dtor,
+                                "text_near_edge/vertical.png")

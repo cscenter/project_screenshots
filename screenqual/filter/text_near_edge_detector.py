797eebb5a7e3b97e167300c7444cb130bb935763
Vladimir Nazarenko <nazarenko2010@gmail.com>
2018-04-06 14:41:18 +0300

    Cleans the test set

--- screenqual/filter/text_near_edge_detector.py
+++ screenqual/filter/text_near_edge_detector.py
@@ -5,17 +5,20 @@ from screenqual.core.analyser_result import AnalyserResult
 
 
 class TextNearEdgeDetector(ScreenshotAnalyser):
-    tolerance = 0.01
-    frame_height = 2
-    min_pixels_in_line_ratio = 0.01
+    tolerance = 0.2
+    frame_height = 10
+    line_eps = 10
 
     def execute(self, screenshot):
         img = screenshot.image
-        assert img.shape[0] > 2 * self.frame_height, "Img is too narrow (height is {0} pixels)".format(img.shape[0])
+        assert min(*img.shape[:2]) > 2 * self.frame_height, "Img is too narrow (size is {0} pixels)".format(img.shape)
         img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-        img = 255 - img[..., 2]
-        has_text_near_horizontal_edge = self._check_horizontal_edges(img)
-        has_text_near_vertical_edge   = self._check_vertical_edges(img)
+        img_max_intensity = np.percentile(img, 99)
+        img = img_max_intensity - img[..., 2]
+        decay_mask = self._exp_decay(img.shape)
+        decayed_img = np.multiply(img, decay_mask)
+        has_text_near_horizontal_edge = self._check_edges(img, decayed_img, work_axis=1)
+        has_text_near_vertical_edge   = self._check_edges(img, decayed_img, work_axis=0)
 
         if has_text_near_horizontal_edge and has_text_near_vertical_edge:
             return AnalyserResult.with_anomaly({"cause": "Horizontal and vertical border crosses the text"})
@@ -26,18 +29,23 @@ class TextNearEdgeDetector(ScreenshotAnalyser):
         else:
             return AnalyserResult.without_anomaly()
 
-    def _check_horizontal_edges(self, img):
-        row_sums = np.sum(img, axis=1)
-        min_pixels_in_line = img.shape[1] * self.min_pixels_in_line_ratio
-        median_row_sum = np.median(row_sums[row_sums > min_pixels_in_line])
-        head = row_sums[:self.frame_height]
-        tail = row_sums[-self.frame_height:]
-        return max(np.max(head), np.max(tail)) > self.tolerance * median_row_sum
+    @staticmethod
+    def _exp_decay(shape):
+        assert len(shape) == 2, "Only 2-dimensional mat is supported"
+        edges_matrix = np.ones(shape, np.uint8)
+        edges_matrix[[0, -1], :] = 0
+        edges_matrix[:, [0, -1]] = 0
+        distance_transform = cv2.distanceTransform(edges_matrix, cv2.DIST_L1, cv2.DIST_MASK_3)
+        return 1 / np.exp(np.minimum(distance_transform.astype(np.float32), 30.))
 
-    def _check_vertical_edges(self, img):
-        col_sums = np.sum(img, axis=0)
-        min_pixels_in_col = img.shape[0] * self.min_pixels_in_line_ratio
-        median_col_sum = np.median(col_sums[col_sums > min_pixels_in_col])
-        head = col_sums[:self.frame_height]
-        tail = col_sums[-self.frame_height:]
-        return max(np.max(head), np.max(tail)) > self.tolerance * median_col_sum
+    def _check_edges(self, original_img, decayed_img, work_axis):
+        assert len(original_img.shape) == 2 and len(decayed_img.shape) == 2
+
+        line_sums = np.sum(original_img, axis=work_axis)
+        min_pixels_in_line = np.percentile(line_sums, 5)
+        median_line_sum = np.median(line_sums[line_sums > min_pixels_in_line + self.line_eps])
+        decayed_line_sums = np.sum(decayed_img, axis=work_axis)
+        head = decayed_line_sums[:self.frame_height]
+        tail = decayed_line_sums[-self.frame_height:]
+
+        return max(np.max(head), np.max(tail)) > self.tolerance * median_line_sum

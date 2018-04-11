dfaf1a17220840ee44b0aa412a88018e5b11d98a
Vladimir Nazarenko <nazarenko2010@gmail.com>
2018-04-11 17:22:59 +0300

    Adds detector of too small images

---
+++ tests/unit/min_size_checker/test_min_size_checker.py
@@ -0,0 +1,19 @@
+from screenqual.filter.min_size_checker import MinSizeChecker
+from tests.unit.unit_test import UnitTest
+
+
+class TestGeneralDetector(UnitTest):
+    def setUp(self):
+        self.analyser = MinSizeChecker()
+
+    def test_does_not_fire_on_good_screenshot(self):
+        self.assert_no_anomaly("min_size_checker/good.png")
+
+    def test_fires_on_tiny_screenshot(self):
+        self.assert_has_anomaly("min_size_checker/tiny.png")
+
+    def test_fires_on_1x1_screenshot(self):
+        self.assert_has_anomaly("min_size_checker/1x1.png")
+
+    def test_fires_on_misread_screenshot(self):
+        self.assert_has_anomaly("min_size_checker/no_such_file.png")

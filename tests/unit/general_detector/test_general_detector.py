6f06ca5ec1d5174ddd519145382d864326e6da9b
Vladimir Nazarenko <nazarenko2010@gmail.com>
2018-04-11 18:48:04 +0300

    Raises threshold to remove FPs

--- tests/unit/general_detector/test_general_detector.py
+++ tests/unit/general_detector/test_general_detector.py
@@ -12,12 +12,15 @@ class TestGeneralDetector(UnitTest):
     def test_does_not_fire_on_good_screenshot2(self):
         self.assert_no_anomaly("general_detector/good2.png")
 
+    def test_does_not_fire_on_white_areas(self):
+        self.assert_no_anomaly("general_detector/lotofwhite.png")
+
+    def test_does_not_fire_on_yandex_main_screenshot(self):
+        self.assert_no_anomaly("general_detector/yndx_main.png")
+
     def test_fires_on_mostly_white_screenshot(self):
         self.assert_has_anomaly("general_detector/mostly_white.png")
 
     def test_fires_on_white_screenshot(self):
         self.assert_has_anomaly("general_detector/white.png")
-
-    def test_fires_on_yandex_main_screenshot(self):
-        self.assert_has_anomaly("general_detector/yndx_main.png")
 

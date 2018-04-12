ac65734a05451cd07bf01662fc6eb6878216b89c
Fenicheva Sofia <alicadr.032@gmail.com>
2018-04-12 13:44:10 +0300

    update banner_detector

--- tests/regression/banner/test_regression_banner_detector.py
+++ tests/regression/banner/test_regression_banner_detector.py
@@ -3,6 +3,6 @@ from tests.regression.regression_test import TestRegression
 
 
 class TestRegressionBanner(TestRegression):
-    def test_fscore_on_broken_images(self):
+    def test_fscore_banner_images(self):
         self.fscore(["/banner/with_banner/"], ["/banner/without_banner/"],
                     BannerAnalyser(), .8, "banner detector", extension="jpg")

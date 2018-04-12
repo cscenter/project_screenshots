075842ec1c13f7cc7a9360854f649dd9cc85ce33
Fenicheva Sofia <alicadr.032@gmail.com>
2018-04-12 13:27:46 +0300

    update banner_detector

--- screenqual/filter/banner_detector.py
+++ screenqual/filter/banner_detector.py
@@ -13,11 +13,10 @@ class BannerAnalyser(ScreenshotAnalyser):
         self.model.load_weights(path_to_model)
 
     def execute(self, screenshot):
-        img_w = screenshot.image.shape[1]//3
-        img = cv2.resize(screenshot.image[:img_w, :], (100, 100)).reshape((1, 100, 100, 3))/255
+        img_w = screenshot.image.shape[1] // 3
+        img = cv2.resize(screenshot.image[:img_w, :], (100, 100)).reshape((1, 100, 100, 3)) / 255
         banner_probability = self.model.predict(img)[0]
         is_banner = banner_probability.argmax(axis=0)
-        print(banner_probability, is_banner)
         if is_banner:
             return AnalyserResult.with_anomaly({"probability": banner_probability[1]})
         return AnalyserResult.without_anomaly({"probability": banner_probability[0]})

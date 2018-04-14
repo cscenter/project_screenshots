28db031b5b462eb4d5cb510f9f894a69c0f463e9
amosov-f <amosov-f@yandex-team.ru>
2018-04-14 22:52:21 +0300

    model files are not in pip

--- setup.py
+++ setup.py
@@ -6,16 +6,5 @@ setup(
     description="Quality of SERP Screenshots",
     packages=find_packages(exclude=("tests",)),
     install_requires=['numpy', 'opencv-python'],
-    package_data={
-        '': [
-            'models/general_detector/avg_spectre.npy',
-            'models/general_detector/spectre_indices.npy',
-            'models/general_detector/threshold.txt'
-        ],
-        'screenqual': [
-            'models/general_detector/avg_spectre.npy',
-            'models/general_detector/spectre_indices.npy',
-            'models/general_detector/threshold.txt'
-        ]
-    },
+    include_data_files=True
 )

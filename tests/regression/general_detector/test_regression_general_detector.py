8bb89fcbee57320897819699b4febaab67609690
Vladimir Nazarenko <nazarenko2010@gmail.com>
2018-04-16 23:09:55 +0300

    Adds mobile tests for general detector

--- tests/regression/general_detector/test_regression_general_detector.py
+++ tests/regression/general_detector/test_regression_general_detector.py
@@ -7,3 +7,8 @@ class TestRegressionGeneralDetector(TestRegression):
         self.fscore(["/general_fullpage/desktop/bad/"],
                     ["/general_fullpage/desktop/good/"],
                     GeneralDetector(), .9, "general detector: desktop")
+
+    def test_fscore_on_mobile_screenshots(self):
+        self.fscore(["/general_fullpage/mobile/bad/"],
+                    ["/general_fullpage/mobile/good/"],
+                    GeneralDetector(), .9, "general detector: mobile", "jpg")

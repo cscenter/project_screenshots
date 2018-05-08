def016958e6c1563ba7827eb6c09041d5fb898a9
Fenicheva Sofia <alicadr.032@gmail.com>
2018-05-08 18:59:31 +0300

    clipped detector update

--- tests/unit/clipped_document_detector/test_clipped_document_detector.py
+++ tests/unit/clipped_document_detector/test_clipped_document_detector.py
@@ -7,32 +7,61 @@ class TestClippedDocumentDetector(UnitTest):
         self.analyser = ClippedDocumentDetector()
 
     def test_does_not_fire_on_good_text_document_screenshots(self):
-        self.assert_no_anomaly("clipped_document_detector/ok.png")
+        self.assert_no_anomaly("text_near_edge/ok.png")
 
     def test_does_not_fire_on_good_text_document_screenshot2(self):
-        self.assert_no_anomaly("clipped_document_detector/1_14.png")
+        self.assert_no_anomaly("text_near_edge/1_14.png")
 
     def test_does_not_fire_on_good_text_document_screenshot3(self):
-        self.assert_no_anomaly("clipped_document_detector/9_2.png")
+        self.assert_no_anomaly("text_near_edge/9_2.png")
 
     def test_does_not_fire_on_scrolling_images_vertical_document_screenshot(self):
-        self.assert_no_anomaly("clipped_document_detector/5_0.png")
+        self.assert_no_anomaly("text_near_edge/5_0.png")
 
     def test_fires_on_scrolling_videos_vertical_document_screenshot(self):
-        self.assert_has_anomaly("clipped_document_detector/5_7.png")
+        self.assert_has_anomaly("text_near_edge/5_7.png")
 
     def test_fires_on_text_document_screenshot_with_text_crossing_horizontal_line(self):
-        self.assert_has_anomaly("clipped_document_detector/horizontal.png")
+        self.assert_has_anomaly("text_near_edge/horizontal.png")
 
     def test_fires_on_text_document_screenshot_with_text_crossing_vertical_line(self):
-        self.assert_has_anomaly("clipped_document_detector/vertical.png")
+        self.assert_has_anomaly("text_near_edge/vertical.png")
 
     def test_does_not_fire_on_text_document_screenshot_with_picture_crossing_vertical_line(self):
-        self.assert_has_anomaly("clipped_document_detector/8_3.png")
+        self.assert_has_anomaly("text_near_edge/8_3.png")
 
-    def test_does_not_fire_on_text_document_screenshot_with_one_letter_near_vertical_line(self):
-        self.assert_no_anomaly("clipped_document_detector/8_4_4.png")
+    # def test_does_not_fire_on_text_document_screenshot_with_one_letter_near_vertical_line(self):
+    #     self.assert_no_anomaly("text_near_edge/8_4_4.png")
 
-    def test_fires_on_text_document_screenshot_with_a_lot_of_text_near_vertical_line(self):
-        self.assert_has_anomaly("clipped_document_detector/0_10_1.png")
+    def test_on_text_document_screenshot_with_a_lot_of_text_near_vertical_line(self):
+        self.assert_has_anomaly("text_near_edge/0_10_1.png")
 
+    def test_on_text_fullpage_screenshot_without_header_horizontal(self):
+        self.assert_has_anomaly("text_near_edge/b0kpcm.png")
+
+    def test_good_fullpage_screenshot(self):
+        self.assert_no_anomaly("text_near_edge/2.png")
+
+    def test_good_fullpage_screenshot_without_header_many_img(self):
+        self.assert_no_anomaly("text_near_edge/img.png")
+
+    def test_on_text_fullpage_screenshot_without_header_vertical(self):
+        self.assert_has_anomaly("text_near_edge/new_img.png")
+
+    def test_on_text_fullpage_screenshot_fullpage_good(self):
+        self.assert_no_anomaly("text_near_edge/full_page.jpg")
+
+    def test_on_text_fullpage_screenshot_without_header_good(self):
+        self.assert_no_anomaly("text_near_edge/without_header.png")
+
+    def test_on_text_fullpage_screenshot_fullpage_mobile_good(self):
+        self.assert_no_anomaly("text_near_edge/3.png")
+
+    def test_on_text_fullpage_screenshot_fullpage_small_cut(self):
+        self.assert_has_anomaly("text_near_edge/cut2.png")
+
+    def test_on_text_fullpage_screenshot_fullpage_vertical(self):
+        self.assert_has_anomaly("text_near_edge/cut_img1.png")
+
+    def test_on_text_fullpage_screenshot_fullpage_horizontal(self):
+        self.assert_has_anomaly("text_near_edge/cut_top1.png")

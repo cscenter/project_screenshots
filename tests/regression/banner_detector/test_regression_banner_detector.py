638af1fb1bcc78f33cb227ac19b8a25dcffd12a2
Fenicheva Sofia <alicadr.032@gmail.com>
2018-05-08 19:08:58 +0300

    add image

--- tests/regression/banner_detector/test_regression_banner_detector.py
+++
@@ -1,8 +0,0 @@
-from screenqual.filter.banner_detector import BannerAnalyser
-from tests.regression.regression_test import TestRegression
-
-
-class TestRegressionBanner(TestRegression):
-    def test_fscore_banner_images(self):
-        self.fscore(["/banner_detector/with_banner/"], ["/banner_detector/without_banner/"],
-                    BannerAnalyser(), .0, "banner_detector detector", extensions=["jpg"])

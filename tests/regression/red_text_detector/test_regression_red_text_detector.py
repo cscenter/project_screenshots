14ce89a3eecad6f1ea97e9e095e7bb6c4baac184
Fenicheva Sofia <alicadr.032@gmail.com>
2018-05-14 00:54:32 +0300

    rename

--- tests/regression/red_text_detector/test_regression_red_text_detector.py
+++ tests/regression/red_text_detector/test_regression_red_text_detector.py
@@ -4,12 +4,12 @@ from tests.regression.regression_test import TestRegression
 
 class TestRegressionRedTextDetector(TestRegression):
 
-    def test_fscore_on_desktop_text_documents(self):
+    def test_fscore_red_text_on_desktop(self):
         self.fscore(["/red_text/desktop/bad/"],
                     ["/red_text/desktop/good/"],
                     RedTextDetector(), .9, "red text detector: desktop", extensions=["png", "jpg"])
 
-    def test_fscore_on_desktop_mobile(self):
+    def test_fscore_red_text_on_mobile(self):
         self.fscore(["/red_text/mobile/bad/"],
                     ["/red_text/mobile/good/"],
                     RedTextDetector(), .9, "red text detector: mobile", extensions=["png", "jpg"])
